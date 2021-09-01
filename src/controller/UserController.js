4dcc43a896cb05dd7739ff578b6c304ca9a22eb3
Kauã Alves <kauaAlvesWorkplace@gmail.com>
2021-09-01 20:02:39 -0300

    resolution

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,84 +1,112 @@
-class UserController{
-    
-    constructor(formId, tableId){
+class UserController {
+
+    constructor (formId, tableId){
+
         this.formEl = document.getElementById(formId);
         this.tableEl = document.getElementById(tableId);
 
-        this.onSubmit();
+        this.onSubmit()
+
     }
 
     onSubmit(){
-        this.formEl.addEventListener("submit", event=>{
+
+        this.formEl.addEventListener("submit", event => {
+
             event.preventDefault();
 
             let values = this.getValues();
 
-            this.getPhoto()
-            .then((content)=>{
-                values.photo = content;
-                this.addUserLine(values);
-            }),(e)=>{
-                console.log(e);
-            }
-        })
+            this.getPhoto().then(
+                (content) => {
+
+                    values.photo = content;
+
+                    this.addLine(values);
+
+                }, 
+                (e) => {
+                    console.error(e)
+                }
+            );
+        
+        });
+
     }
 
     getPhoto(){
 
         return new Promise((resolve, reject) => {
-            
-            //api :
+
             let fileReader = new FileReader();
-    
-            let elements = [...this.formEl.elements].filter(item =>{
-                if(item.name === 'photo'){
+
+            let elements = [...this.formEl.elements].filter(item => {
+
+                if (item.name === 'photo') {
                     return item;
                 }
-            });
-    
-            let file = elements[0].files[0];
-            
-            //quando carregar:
 
-            fileReader.onLoad = ()=>{
+            });
+
+            let file = elements[0].files[0];
+
+            fileReader.onload = () => {
+
                 resolve(fileReader.result);
+
             };
-            fileReader.onerror = (e)=> {
+
+            fileReader.onerror = (e) => {
+
                 reject(e);
+
             };
+
             fileReader.readAsDataURL(file);
-        })
+
+        });
+
     }
 
     getValues(){
+
         let user = {};
 
-        [...this.formEl.elements].forEach(function (field){
-            if(field.name == "gender") {
-                if(field.checked){
-                    user[field.name] = field.value;
+        [...this.formEl.elements].forEach(function(field, index){
+
+            if (field.name === "gender") {
+    
+                if (field.checked) {
+                    user[field.name] = field.value
                 }
-            }else{
-                user[field.name] = field.value;
+    
+            } else {
+    
+                user[field.name] = field.value
+    
             }
+    
         });
-
+    
         return new User(
-            user.name,
-            user.gender,
-            user.birth,
-            user.country,
-            user.email,
-            user.password,
-            user.photo,
-            user.admin,
+            user.name, 
+            user.gender, 
+            user.birth, 
+            user.country, 
+            user.email, 
+            user.password, 
+            user.photo, 
+            user.admin
         );
-    };
 
-    addUserLine(dataUser){
-        this.tableEl.innerHTML = 
-            `<tr>
-                <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
+    }
+
+    
+    addLine(dataUser) {
+
+        this.tableEl.innerHTML = `
+            <tr>
+                <td><img src=${dataUser.photo} class="img-circle img-sm"></td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
                 <td>${dataUser.admin}</td>
@@ -86,8 +114,10 @@ class UserController{
                 <td>
                     <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                     <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                </td>    
-            <tr>`;
+                </td>
+            </tr>
+        `;
+
     }
 
 
