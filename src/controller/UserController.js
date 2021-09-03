1058564402c7c33a1fe6c592c22b3f854c2d0d40
Kauã Alves <kauaAlvesWorkplace@gmail.com>
2021-09-03 18:01:47 -0300

    03/09/21

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -12,18 +12,13 @@ class UserController {
     onSubmit(){
 
         this.formEl.addEventListener("submit", event => {
-
             event.preventDefault();
-
             let values = this.getValues();
 
             this.getPhoto().then(
                 (content) => {
-
                     values.photo = content;
-
-                    this.addLine(values);
-
+                    this.addUserLine(values);
                 }, 
                 (e) => {
                     console.error(e)
@@ -41,28 +36,25 @@ class UserController {
             let fileReader = new FileReader();
 
             let elements = [...this.formEl.elements].filter(item => {
-
                 if (item.name === 'photo') {
                     return item;
                 }
-
             });
 
             let file = elements[0].files[0];
-
             fileReader.onload = () => {
-
                 resolve(fileReader.result);
-
             };
 
             fileReader.onerror = (e) => {
-
                 reject(e);
-
             };
 
-            fileReader.readAsDataURL(file);
+            if(file){
+                fileReader.readAsDataURL(file);   
+            }else{
+                resolve('dist/img/boxed-bg.jpg');
+            }
 
         });
 
@@ -72,18 +64,19 @@ class UserController {
 
         let user = {};
 
-        [...this.formEl.elements].forEach(function(field, index){
-
+        [...this.formEl.elements].forEach(function(field){
             if (field.name === "gender") {
-    
+
                 if (field.checked) {
-                    user[field.name] = field.value
+                    user[field.name] = field.value;
                 }
-    
-            } else {
-    
-                user[field.name] = field.value
-    
+
+            }
+            else if(field.name == "admin"){
+                user[field.name] = field.checked;
+
+            }else {
+                user[field.name] = field.value;
             }
     
         });
@@ -101,24 +94,24 @@ class UserController {
 
     }
 
-    
-    addLine(dataUser) {
+    addUserLine(dataUser) {
 
-        this.tableEl.innerHTML = `
-            <tr>
-                <td><img src=${dataUser.photo} class="img-circle img-sm"></td>
-                <td>${dataUser.name}</td>
-                <td>${dataUser.email}</td>
-                <td>${dataUser.admin}</td>
-                <td>${dataUser.birth}</td>
-                <td>
-                    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                </td>
-            </tr>
-        `;
+        var tr = document.createElement('tr');
+
+        tr.innerHTML = `
+                        <td><img src=${dataUser.photo} class="img-circle img-sm"></td>
+                        <td>${dataUser.name}</td>
+                        <td>${dataUser.email}</td>
+                        <td>${(dataUser.admin) ? 'Sim':'Não'}</td>
+                        <td>${dataUser.birth}</td>
+                        <td>
+                            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                        </td>
+                    `;
+        
+        this.tableEl.appendChild(tr);
 
     }
-
 
 }
